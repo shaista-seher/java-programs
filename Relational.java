bb067ad8e3f172d454b4c03523b30fdeaa31291c
shaista seher <shaistaarshad006@gmail.com>
2025-11-11 15:15:02 +0530

    Create Relational.java

---
+++ Relational.java
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    if (a > b)
+        printf("%d is greater than %d\n", a, b);
+    else if (a < b)
+        printf("%d is greater than %d\n", b, a);
+    else
+        printf("Both numbers are equal.\n");
+
+    return 0;
+}

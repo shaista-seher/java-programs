b48c6bbd5067c8252252f4d37c4405f6552b719b
shaista seher <shaistaarshad006@gmail.com>
2025-11-20 22:09:03 +0530

    Create Cylinder.java

---
+++ Cylinder.java
@@ -0,0 +1,20 @@
+class Cylinder {
+    private int radius;
+    private int height;
+
+    void setRadius(int r) {
+        radius = r;
+    }
+
+    void setHeight(int h) {
+        height = h;
+    }
+
+    int getRadius() {
+        return radius;
+    }
+
+    int getHeight() {
+        return height;
+    }
+}

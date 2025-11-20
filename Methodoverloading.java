6edbf779efa72bedc41f22b4214ce9846d7ef206
shaista seher <shaistaarshad006@gmail.com>
2025-11-20 22:15:30 +0530

    Create Methodoverloading.java

---
+++ Methodoverloading.java
@@ -0,0 +1,19 @@
+class Calculator {
+    int add(int a, int b) {
+        return a + b;
+    }
+    int add(int a, int b, int c) {
+        return a + b + c;
+    }
+    double add(double a, double b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+
+        System.out.println(calc.add(5, 10));        
+        System.out.println(calc.add(5, 10, 15));    
+        System.out.println(calc.add(5.5, 2.5));    
+    }
+}

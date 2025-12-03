83404c6f0b19401e476c3453c684e072a213fd36
shaista seher <shaistaarshad006@gmail.com>
2025-12-03 23:25:02 +0530

    Create Modulus.java

---
+++ Modulus.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class ModulusExample {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        try {
+            System.out.print("Enter first number: ");
+            int a = sc.nextInt();
+
+            System.out.print("Enter second number: ");
+            int b = sc.nextInt();
+
+            int result = a % b;   // may throw ArithmeticException
+            System.out.println("Result = " + result);
+        } catch (ArithmeticException e) {
+            System.out.println("Error: Cannot divide by zero!");
+        }
+    }
+}

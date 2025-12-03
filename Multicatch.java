0141726c112aaecf362dacf01ee07d17da6a6847
shaista seher <shaistaarshad006@gmail.com>
2025-12-03 23:26:13 +0530

    Create Multicatch.java

---
+++ Multicatch.java
@@ -0,0 +1,13 @@
+public class Muilticatch {
+
+  public static void main(String[] args) {
+    try {
+      int result = 10 / 0;
+      int[] numbers = {1, 2, 3};
+      System.out.println(numbers[10]);
+    } 
+    catch (ArithmeticException | ArrayIndexOutOfBoundsException e) {
+      System.out.println("Math error or array error occurred.");
+    }
+  }
+}
